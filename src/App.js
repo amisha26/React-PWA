21996a0073a0b9cf7c97bf6689b8f9a4938d7beb
amisha26 <amisha8299@gmail.com>
2021-09-10 10:37:47 +0530

    app.js done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import "./App.css";
 
 function App() {
   const [cityName, setCityName] = useState("");
+  const [checkWeather, setCheckWeather] = useState({});
 
   // on key press enter we will call the weather api
   // using the search func
   const search = async (e) => {
     if (e.key === "Enter") {
       const data = await Weather(cityName);
-      console.log(data);
+      //console.log(data);
+      setCheckWeather(data);
+      setCityName("");
     }
   };
 
@@ -24,6 +27,27 @@ function App() {
         onChange={(e) => setCityName(e.target.value)}
         onKeyPress={search}
       />
+      {checkWeather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{checkWeather.name}</span>
+            <sup>{checkWeather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(checkWeather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          {/* for getting the weather img  */}
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${checkWeather.weather[0].icon}@2x.png`}
+              alt={checkWeather.weather[0].description}
+            />
+            <p>{checkWeather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
