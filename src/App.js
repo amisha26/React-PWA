1673c207dbee1eb996b153fa0c7911e2edc3bdb8
amisha26 <amisha8299@gmail.com>
2021-09-10 10:06:56 +0530

    App.js setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
+import { Weather } from "./comp/Weather";
+import "./App.css";
+
 function App() {
-  return (
-    <div className="App">
-      React PWA
-    </div>
-  );
+  return <div className="App">React PWA</div>;
 }
 
 export default App;
